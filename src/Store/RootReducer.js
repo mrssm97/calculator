5daf7a5259528d8af8f8354ed38d67d17d8c5fd0
Sanjay Sharan Maurya <143677520+mrssm97@users.noreply.github.com>
2023-11-25 19:56:37 +0530

    Delete src directory

--- src/Store/RootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import TodoReducer from "./TodoReducer";
-
-const rootReducer = combineReducers({
-  TodoState: TodoReducer,
-});
-export default rootReducer;
